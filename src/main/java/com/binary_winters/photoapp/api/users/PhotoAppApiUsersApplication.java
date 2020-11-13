d87cd9c2957ea74e5f7be87adc2e38718be56d4a
chipo <pnapoli33@gmail.com>
2020-11-13 15:36:59 -0300

    video 42.0 - Add @EnableDiscoveryClient and properties.

--- src/main/java/com/binary_winters/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/binary_winters/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,8 +2,11 @@ package com.binary_winters.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// This will make this Spring Boot application a client which will be communicating with the discovery server.
+@EnableDiscoveryClient
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
