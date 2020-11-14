f7977faf83eb347416913a1e9f2e943ccd5358ff
chipo <pnapoli33@gmail.com>
2020-11-14 19:20:11 -0300

    video 60-61 - Add @RequestBody and @Valid to createUser() method and a new class for receive the payload from the request called CreateUserRequestModel with its validations.

--- src/main/java/com/binary_winters/photoapp/api/users/ui/controllers/UserController.java
+++ src/main/java/com/binary_winters/photoapp/api/users/ui/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.binary_winters.photoapp.api.users.ui.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.binary_winters.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -20,7 +25,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser() {
+	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		return "Create user method is called";
 	}
 
