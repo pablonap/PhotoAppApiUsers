0cb453f70f1a352c7ced4daaa647b254d202747e
chipo <pnapoli33@gmail.com>
2020-11-14 19:09:23 -0300

    video 59.0 - Add basic method for createUser() in the controller.

--- src/main/java/com/binary_winters/photoapp/api/users/ui/controllers/UserController.java
+++ src/main/java/com/binary_winters/photoapp/api/users/ui/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.binary_winters.photoapp.api.users.ui.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,11 @@ public class UserController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port " + env.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "Create user method is called";
 	}
 
 }
