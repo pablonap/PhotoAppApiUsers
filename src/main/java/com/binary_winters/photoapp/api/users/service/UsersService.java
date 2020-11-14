18fce7e1d1fe875812e72163d7c144c61998e4e6
chipo <pnapoli33@gmail.com>
2020-11-14 19:51:25 -0300

    video 63-64 - Create UserDto class; UsersService interface; and UsersServiceImpl

---
+++ src/main/java/com/binary_winters/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.binary_winters.photoapp.api.users.service;
+
+import com.binary_winters.photoapp.api.users.shared.UserDto;
+
+public interface UsersService {
+	
+	UserDto createUser(UserDto userDetails);
+
+}
---
+++ src/main/java/com/binary_winters/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,13 @@
+package com.binary_winters.photoapp.api.users.service;
+
+import com.binary_winters.photoapp.api.users.shared.UserDto;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
