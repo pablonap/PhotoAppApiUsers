76635f493315078b8036744d6d2e31692b89d0ae
chipo <pnapoli33@gmail.com>
2020-11-17 12:06:56 -0300

    video 70.0 - Return Http status code 201 for having created a new resource.

--- src/main/java/com/binary_winters/photoapp/api/users/ui/controllers/UserController.java
+++ src/main/java/com/binary_winters/photoapp/api/users/ui/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,7 +34,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+	public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
@@ -41,7 +43,7 @@ public class UserController {
 		
 		usersService.createUser(userDto);
 
-		return "Create user method is called";
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
 
 }
